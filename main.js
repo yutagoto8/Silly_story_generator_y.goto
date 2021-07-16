40c195c072d76d6e11169fde49f81bc435c08d2a
Y.Goto <y.goto@cybermissions.co.jp>
2021-07-16 14:43:03 +0900

    「result() 関数の作成」の終了

--- main.js
+++ main.js
@@ -15,22 +15,40 @@ let insertY = ['the soup kitchen','Disneyland','the White House'];
 
 let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
-
+//resultを実行。何回も変えるためnewStoryでワンクッションおいている
 randomize.addEventListener('click', result);
 
 function result() {
+  let newStory = storyText;
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  // newStory = newStory.replace(':insertx:',xItem);
+  // newStory = newStory.replace(':inserty:',yItem);
+  // newStory = newStory.replace(':insertz:',zItem);
+  // newStory = newStory.replace(':insertx:',xItem);
+  //下記のように一括で書ける
+  newStory = newStory
+     .replace(':insertx:',xItem)
+     .replace(':inserty:',yItem)
+     .replace(':insertz:',zItem)
+     .replace(':insertx:',xItem)
 
   if(customName.value !== '') {
     let name = customName.value;
-
+    newStory = newStory.replace("Bob",name);
   }
 
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature =  Math.round(94);
+    let weight = Math.round(300 * 0.071429) + ' stone';
+    let temperature =  Math.round( 5/9 * (94 -32)) + ' setigrade';
 
+    newStory = newStory.replace('94 fahrenheit',temperature);
+    newStory = newStory.replace('300 pounds',weight);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
